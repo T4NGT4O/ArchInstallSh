37c00631b5bdf9de5f780ea767725b0b103a67f8
T4NGT4O <tangtaotaopku@126.com>
2017-10-05 12:12:13 +0800

    simple version

---
+++ scfg.sh
@@ -0,0 +1,49 @@
+#set time
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock --systohc --utc
+#set languages
+echo 'zh_CN.UTF-8
+zh_HK.UTF-8
+zh_TW.UTF-8
+en_US.UTF-8' > /etc/locale.gen
+locale-gen
+#set console language
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+#set host name
+echo "MyArch" > /etc/hostname
+#set hosts 
+echo "127.0.1.1    MyArch.localdomain    MyArch" >> /etc/hosts
+#change root passwd
+#not suitable for github version
+
+#install bootloader
+echo "Y
+" | pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Y
+" | pacman -S sudo
+
+
+#pacman -S xorg
+
+#pacman -S plasma-desktop
+#pacman -S plasma
+
+
+#pacman -S kde-applications
+
+
+#pacman -S kde-l10n-zh_cn
+
+
+#pacman -S sddm
+
+
+#systemctl enable sddm
+#systemctl disable netctl
+#systemctl enable NetworkManager
+
+useradd -m -G wheel me
+read -p "any key..." tmp
---
+++ ssetup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#partition about 42G
+#make a gpt table
+echo "
+g
+n
+1
+
++512M
+t
+1
+n
+2
+
++32G
+t
+2
+24
+n
+3
+
++8G
+t
+3
+28
+w
+q
+" | fdisk /dev/sda
+#mkfs
+mkfs.fat -F32 /dev/sda1
+mkfs.xfs /dev/sda2
+mkfs.xfs /dev/sda3
+#mount
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mkdir /mnt/home
+mount /dev/sda1 /mnt/boot
+mount /dev/sda2 /mnt/boot
+#change mirrorlist
+#wget -O /etc/pacman.d/mirrorlist https://www.archlinux.org/mirrorlist/?country=CN
+#pacman -S reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
+echo 'Server = http://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch
+Server = http://mirror.bjtu.edu.cn/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+pacstrap /mnt base
+
+genfstab -U /mnt >> /mnt/etc/fstab
+mkdir /mnt/root
+wget https://github.com/T4NGT4O/ArchInstallSh/raw/master/scfg.sh 
+vim scfg.sh
+read -p "any key..." tmp
+mv scfg.sh /mnt/root/sconfig.sh
+chmod +x /mnt/root/sconfig.sh
+arch-chroot /mnt /root/sconfig.sh
