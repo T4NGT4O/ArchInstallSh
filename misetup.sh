cf43b36d55a9eaf7a6fc9af70df8159eefed1b34
T4O <29798577+T4NGT4O@users.noreply.github.com>
2018-05-30 21:29:26 +0800

    Update misetup.sh
    delete original linux file in EFI

--- misetup.sh
+++ misetup.sh
@@ -16,6 +16,12 @@ read -p 'any key...' tmp
 mount /dev/nvme0n1p6 /mnt
 mkdir /mnt/boot
 mount /dev/nvme0n1p2 /mnt/boot
+# delete original linux/grub
+rm -rf /mnt/boot/grub
+rm -f /mnt/boot/*.img
+rm -f /mnt/boot/vmlinuz-linux
+rm -rf /mnt/boot/EFI/Arch
+
 #change mirrorlist
 #wget -O /etc/pacman.d/mirrorlist https://www.archlinux.org/mirrorlist/?country=CN
 #pacman -S reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
